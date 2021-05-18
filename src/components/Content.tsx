09b34e15f6c8ec2bb21062509f672f96a61a92df
Ysler <ysler@metisagenciadigital.com.br>
2021-05-18 00:10:58 -0300

    complete

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,24 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+import '../styles/content.scss';
+
+export function Content({ title, movies }) {
+ return(
+  
+
+  <main>
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> { title } </span></span>
+        </header>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </div>
+
+    
+  </main>
+ );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,23 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+
+import '../styles/sidebar.scss';
+
+export function SideBar({genres, selectedGenreId, handleClickButton}) {
+  
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+  );
 }
